d770bfe891fd89ebbc0fa1b43bd93b5c935c1463
Jonathan Moore <jonathanmoore@Jonathans-MBP-2.lan>
2018-10-28 15:15:32 +0000

    Final cocommit

--- jenna-buchholz/final-code/scroll.js
+++ jenna-buchholz/final-code/scroll.js
@@ -51,21 +51,21 @@ document.addEventListener("scroll", function () {
 // what is the anchor? well its the middle of the section
 // how far should we parallax? well, it's a ratio of the middle distance scrolled to the middle point of the anchor
 document.addEventListener("scroll", function () {
-  const topViewport = window.pageYOffset
-  const midViewport = topViewport + (window.innerHeight / 2)
-  
-  sections.forEach(section => {
-    const topSection = section.offsetTop
-    const midSection = topSection + (section.offsetHeight / 2)
+    const topViewport = window.pageYOffset
+    const midViewport = topViewport + (window.innerHeight / 2)
     
-    const distanceToSection = midViewport - midSection
-    
-    const parallaxTags = section.querySelectorAll(`[data-parallax]`)
-    
-    // loop over each parallaxed tag
-    parallaxTags.forEach(tag => {
-      const speed = parseFloat(tag.getAttribute("data-parallax"))
-      tag.style.transform = `translate(0, ${distanceToSection * speed}px)`
+      sections.forEach(section => {
+        const topSection = section.offsetTop
+        const midSection = topSection + (section.offsetHeight / 2)
+        
+        const distanceToSection = midViewport - midSection
+        
+        const parallaxTags = section.querySelectorAll(`[data-parallax]`)
+        
+        // loop over each parallaxed tag
+        parallaxTags.forEach(tag => {
+          const speed = parseFloat(tag.getAttribute("data-parallax"))
+          tag.style.transform = `translate(0, ${distanceToSection * speed}px)`
+        })
     })
-  })
 })
--- js/scroll.js
+++ js/scroll.js
@@ -4,6 +4,7 @@ const progressTag = document.querySelector("div.progress")
 const sections = document.querySelectorAll("section")
 const clientTag = document.querySelector("h3.client")
 const pageTag = document.querySelector("div.pagination")
+const headerTag = document.querySelector("header")
 
 // when i scroll the page, update the pixels tag to be how far
 // we've scrolled
@@ -33,10 +34,43 @@ document.addEventListener("scroll", function () {
     const pixels = window.pageYOffset
 
     sections.forEach(section => {   // for each section, check to see if we've passed it
-        if (section.offsetTop < pixels) {
-            clientTag.innerHTML = "Test123"
-            pageTag.innerHTML = "3 / 4"
+        if (section.offsetTop - 150 <= pixels) {
+            clientTag.innerHTML = section.getAttribute ("data-client")
+            pageTag.innerHTML = section.getAttribute("data-pagination")
+
+            if (section.hasAttribute("data-is-dark")) {
+                headerTag.classList.add("white")
+                progressTag.classList.add("white")
+            }
+            else {
+                headerTag.classList.remove("white")
+                progressTag.classList.remove("white")
+            }
         }
     })
+})
 
+// when we scroll the page, make things parallax
+// we want to move certain tags based on how far they are from an anchor point
+// what is the anchor? it's the middle of the section
+// how far should we parallax? A ratio of the middle distance scrolled to the middle point of the anchor
+// when we're in the middle of the page, the shapes are in the right place
+document.addEventListener("scroll", function () {
+    const topViewport = window.pageYOffset
+    const midViewport = topViewport + (window.innerHeight / 2)
+    
+        sections.forEach(section => {
+            const topSection = section.offsetTop
+            const midSection = topSection + (section.offsetHeight / 2)
+
+            const distanceToSection = midViewport - midSection
+
+            const parallaxTags = section.querySelectorAll(`[data-parallax]`) // get any tag with data-parallax
+
+            // loop over eached parallaxed tag
+            parallaxTags.forEach(tag => {
+                const speed = parseFloat(tag.getAttribute("data-parallax")) // parseFloat turn string into decimal number
+                tag.style.transform = `translate(0, ${distanceToSection * speed}px)`
+        })
+    })
 })
